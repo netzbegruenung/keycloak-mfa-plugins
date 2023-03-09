858ad594100654e971b29ac7061f5e3f1331a4f8
Giuliano Mele <giuliano.mele@verdigado.com>
2023-03-09 17:50:26 +0100

    Update credential model
    - Remove secret data
    - Add public key, device id, device OS to credential data

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/credentials/AppCredentialData.java
+++ app-authenticator/src/main/java/netzbegruenung/keycloak/app/credentials/AppCredentialData.java
@@ -6,12 +6,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class AppCredentialData {
 	private final String publicKey;
 
+	private final String devicedId;
+	private final String deviceOs;
+
 	@JsonCreator
-	public AppCredentialData(@JsonProperty("publicKey") String publicKey) {
+	public AppCredentialData(
+		@JsonProperty("publicKey") String publicKey,
+		@JsonProperty("deviceId") String devicedId,
+		@JsonProperty("deviceOs") String deviceOs
+	) {
 		this.publicKey = publicKey;
+		this.devicedId = devicedId;
+		this.deviceOs = deviceOs;
 	}
 
 	public String getPublicKey() {
 		return publicKey;
+	}
+
+	public String getDevicedId() {
+		return devicedId;
+	}
+
+	public String getDeviceOs() {
+		return deviceOs;
 	}
 }
