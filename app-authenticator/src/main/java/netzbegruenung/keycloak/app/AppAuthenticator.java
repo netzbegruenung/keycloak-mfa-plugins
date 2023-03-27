a12577a77c331ba0f2a3aed3c57f325c113d4db1
Giuliano Mele <giuliano.mele@verdigado.com>
2023-03-27 18:39:24 +0200

    Add app device selection dialog

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/AppAuthenticator.java
+++ app-authenticator/src/main/java/netzbegruenung/keycloak/app/AppAuthenticator.java
@@ -3,14 +3,15 @@ package netzbegruenung.keycloak.app;
 import netzbegruenung.keycloak.app.actiontoken.ActionTokenUtil;
 import netzbegruenung.keycloak.app.actiontoken.AppAuthActionToken;
 import netzbegruenung.keycloak.app.credentials.AppCredentialData;
-import netzbegruenung.keycloak.app.credentials.AppCredentialModel;
 import netzbegruenung.keycloak.app.messaging.MessagingServiceFactory;
 import org.apache.commons.codec.binary.Base64;
 import org.jboss.logging.Logger;
+import org.jetbrains.annotations.Nullable;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.CredentialValidator;
 import org.keycloak.common.util.SecretGenerator;
+import org.keycloak.credential.CredentialModel;
 import org.keycloak.credential.CredentialProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
@@ -27,6 +28,7 @@ import java.security.*;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class AppAuthenticator implements Authenticator, CredentialValidator<AppCredentialProvider> {
@@ -36,13 +38,19 @@ public class AppAuthenticator implements Authenticator, CredentialValidator<AppC
 
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
-		try {
-			AppCredentialModel appCredentialModel = getCredentialProvider(context.getSession())
-				.getDefaultCredential(context.getSession(), context.getRealm(), context.getUser());
-			AppCredentialData appCredentialData = JsonSerialization.readValue(appCredentialModel.getCredentialData(), AppCredentialData.class);
+		CredentialModel appCredentialModel;
+		appCredentialModel = getSelectedAppCredential(context);
+		if (appCredentialModel == null) return;
 
+		createAppChallenge(context, appCredentialModel);
+	}
+
+	private void createAppChallenge(AuthenticationFlowContext context, CredentialModel appCredentialModel) {
+		try {
+			AppCredentialData appCredentialData = JsonSerialization.readValue(appCredentialModel.getCredentialData(), AppCredentialData.class);
 			String secret = SecretGenerator.getInstance().randomString(SECRET_LENGTH, SecretGenerator.ALPHANUM);
 			context.getAuthenticationSession().setAuthNote("secret", secret);
+			context.getAuthenticationSession().setAuthNote("credentialId", appCredentialModel.getId());
 
 			String encryptedSecret = encryptSecretMessage(appCredentialData, secret);
 
@@ -77,6 +85,32 @@ public class AppAuthenticator implements Authenticator, CredentialValidator<AppC
 		}
 	}
 
+	@Nullable
+	private CredentialModel getSelectedAppCredential(AuthenticationFlowContext context) {
+		CredentialModel appCredentialModel;
+		AuthenticationSessionModel authSession = context.getAuthenticationSession();
+		String credentialId = authSession.getAuthNote("selectedCredentialId");
+		authSession.clearAuthNotes();
+
+		if (credentialId == null) {
+			List<CredentialModel> appCredentialModels = getCredentialProvider(context.getSession())
+				.getAllCredentials(context.getUser());
+			if (appCredentialModels.size() > 1) {
+				authSession.setAuthNote("appCredentialSelection", "true");
+				Response challenge = context.form()
+					.setAttribute("appCredentials", appCredentialModels)
+					.createForm("app-auth-selection.ftl");
+				context.challenge(challenge);
+				return null;
+			}
+			appCredentialModel = appCredentialModels.get(0);
+		} else {
+			appCredentialModel = getCredentialProvider(context.getSession())
+				.getCredentialModel(context.getUser(), credentialId);
+		}
+		return appCredentialModel;
+	}
+
 	private String encryptSecretMessage(AppCredentialData credentialData, String secret) throws GeneralSecurityException {
 		KeyFactory keyFactory = KeyFactory.getInstance(credentialData.getAlgorithm());
 		byte[] publicKeyBytes = Base64.decodeBase64(credentialData.getPublicKey());
@@ -93,6 +127,17 @@ public class AppAuthenticator implements Authenticator, CredentialValidator<AppC
 	@Override
 	public void action(AuthenticationFlowContext context) {
 		final AuthenticationSessionModel authSession = context.getAuthenticationSession();
+
+		if (Boolean.parseBoolean(authSession.getAuthNote("appCredentialSelection"))) {
+			authSession.setAuthNote(
+				"selectedCredentialId",
+				context.getHttpRequest().getDecodedFormParameters().getFirst("app-credential")
+			);
+			CredentialModel appCredentialModel = getSelectedAppCredential(context);
+			createAppChallenge(context, appCredentialModel);
+			return;
+		}
+
 		final String granted = authSession.getAuthNote("appAuthGranted");
 		if (granted == null) {
 			Response challenge = context.form()
