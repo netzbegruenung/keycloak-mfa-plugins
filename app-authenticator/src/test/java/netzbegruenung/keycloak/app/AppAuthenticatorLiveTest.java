20deb5f9b56f0053b686e89bab9e85ef24a6fb18
Giuliano Mele <giuliano.mele@verdigado.com>
2023-07-28 11:46:09 +0200

    Fix keycloak dev server and tests

--- app-authenticator/src/test/java/netzbegruenung/keycloak/app/AppAuthenticatorLiveTest.java
+++ app-authenticator/src/test/java/netzbegruenung/keycloak/app/AppAuthenticatorLiveTest.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
