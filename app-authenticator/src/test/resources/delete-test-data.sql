50e3cb9ead7e390f0b275f9a5e94004b31f73373
Giuliano Mele <giuliano.mele@verdigado.com>
2024-06-25 14:59:51 +0200

    Remove broken dev server

--- app-authenticator/src/test/resources/delete-test-data.sql
+++
@@ -1,1 +0,0 @@
-delete from APP_AUTH_CHALLENGE;
--- dev/src/main/java/netzbegruenung/keycloak/dev/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package netzbegruenung.keycloak.dev.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
