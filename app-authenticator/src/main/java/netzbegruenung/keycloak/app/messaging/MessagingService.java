e746816ccad8f71c9f4d3812db7a0ce55bcb9286
Giuliano Mele <giuliano.mele@verdigado.com>
2025-11-11 16:30:45 +0100

    feat(app): add FCM push notification service

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/messaging/MessagingService.java
+++ app-authenticator/src/main/java/netzbegruenung/keycloak/app/messaging/MessagingService.java
@@ -1,8 +1,9 @@
 package netzbegruenung.keycloak.app.messaging;
 
 import netzbegruenung.keycloak.app.dto.ChallengeDto;
+import org.keycloak.models.KeycloakSession;
 
 public interface MessagingService {
 
-	void send(String devicePushId, ChallengeDto challenge);
+	void send(String devicePushId, ChallengeDto challenge, KeycloakSession session);
 }
