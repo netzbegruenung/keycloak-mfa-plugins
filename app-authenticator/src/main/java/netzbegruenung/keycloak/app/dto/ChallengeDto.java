2cf850d22c70cf5f732504b2dc14ba4c73c08949
Giuliano Mele <giuliano.mele@verdigado.com>
2025-11-11 16:16:30 +0100

    refactor(app): format code for improved readability

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/dto/ChallengeDto.java
+++ app-authenticator/src/main/java/netzbegruenung/keycloak/app/dto/ChallengeDto.java
@@ -5,7 +5,20 @@ import java.io.Serializable;
 /**
  * A DTO for the {@link netzbegruenung.keycloak.app.jpa.Challenge} entity
  */
-public record ChallengeDto(String userName, String userFirstName, String userLastName, String targetUrl,
-						   String codeChallenge, Long updatedTimestamp, String ipAddress, String device, String browser,
-						   String os, String osVersion, String clientName, String clientUrl, String loginId) implements Serializable {
+public record ChallengeDto(
+	String userName,
+	String userFirstName,
+	String userLastName,
+	String targetUrl,
+	String codeChallenge,
+	Long updatedTimestamp,
+	String ipAddress,
+	String device,
+	String browser,
+	String os,
+	String osVersion,
+	String clientName,
+	String clientUrl,
+	String loginId
+) implements Serializable {
 }
