9058a1df749fda0ac98a9b27c0c103615d85658e
Giuliano Mele <giuliano.mele@verdigado.com>
2025-12-02 15:26:18 +0100

    chore(app): Remove unused DeviceConnection class
    The `DeviceConnection` class was moved to a nested class in a previous commit and is no longer used.

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/rest/DeviceConnection.java
+++
@@ -1,8 +0,0 @@
-package netzbegruenung.keycloak.app.rest;
-
-import jakarta.ws.rs.container.AsyncResponse;
-
-import java.util.concurrent.ScheduledFuture;
-
-public record DeviceConnection(AsyncResponse asyncResponse, ScheduledFuture<?> evictionJob) {
-}
