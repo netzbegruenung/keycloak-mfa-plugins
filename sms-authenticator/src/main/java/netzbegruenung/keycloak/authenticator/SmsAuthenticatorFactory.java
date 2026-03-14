eb5a2a7ae8271bec7fc2da66b90c12c7af57880c
Christophe Kyvrakidis <christophe@kyvrakidis.com>
2026-03-14 22:00:57 +0100

    feat(intl-tel-input): add smart international phone input and config flags
    - Added enableIntlTelInput flag to enable the feature
    - Implemented support for countryCodeList option
    - Added enableCountryCodeList flag to toggle previous country list feature

--- sms-authenticator/src/main/java/netzbegruenung/keycloak/authenticator/SmsAuthenticatorFactory.java
+++ sms-authenticator/src/main/java/netzbegruenung/keycloak/authenticator/SmsAuthenticatorFactory.java
@@ -99,7 +99,9 @@ public class SmsAuthenticatorFactory implements AuthenticatorFactory {
 			new ProviderConfigProperty("numberTypeFilters", "Valid number type filters", "A list of valid number types to filter the input phone number by. Possible values are: FIXED_LINE, MOBILE, "
 				+ " FIXED_LINE_OR_MOBILE, PAGER, TOLL_FREE, PREMIUM_RATE, SHARED_COST, PERSONAL_NUMBER, VOIP, UAN, VOICEMAIL.", ProviderConfigProperty.MULTIVALUED_STRING_TYPE, Collections.emptyList()),
 			new ProviderConfigProperty("forceRetryOnBadFormat", "Ask for new number if checks fail", "Sets an error message and asks the user to re-enter phone number if formatting checks are not successfully passed.", ProviderConfigProperty.BOOLEAN_TYPE, false),
-			new ProviderConfigProperty("countryCodeList", "List of country code", "Sets the list of country code in a select input to display to the user the supported countries. List separated by commas (ex : FR,DE,GB)", ProviderConfigProperty.STRING_TYPE, "")
+			new ProviderConfigProperty("enableCountryCodeList", "Enable country code list", "Enable the list of all countries", ProviderConfigProperty.BOOLEAN_TYPE, false),
+			new ProviderConfigProperty("enableItelTelInput", "Enable International Telephone Input", "Enable the list of all countries based on International Telephone Input", ProviderConfigProperty.BOOLEAN_TYPE, false),
+			new ProviderConfigProperty("countryCodeList", "List of country code", "Sets the list of country code in a select input to display to the user the supported countries. Possible values are the region Code_ FR, DE, GB", ProviderConfigProperty.MULTIVALUED_STRING_TYPE, Collections.emptyList())
 		);
 	}
 
