d86a90de61771a4f2bb39030850fef6ae88ade6b
timmson <timmson666@mail.ru>
2026-04-30 19:18:08 +0300

    Add tests

--- sms-authenticator/src/test/java/netzbegruenung/keycloak/authenticator/SmsAuthenticatorTest.java
+++ sms-authenticator/src/test/java/netzbegruenung/keycloak/authenticator/SmsAuthenticatorTest.java
@@ -17,6 +17,8 @@
 
 package netzbegruenung.keycloak.authenticator;
 
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import netzbegruenung.keycloak.authenticator.credentials.SmsAuthCredentialModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,14 +26,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
+import org.keycloak.credential.CredentialModel;
 import org.keycloak.credential.CredentialProvider;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.http.HttpRequest;
+import org.keycloak.models.*;
+import org.keycloak.sessions.AuthenticationSessionModel;
 import org.keycloak.theme.Theme;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import static netzbegruenung.keycloak.authenticator.SmsAuthenticator.TPL_CODE;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -61,6 +71,42 @@ class SmsAuthenticatorTest {
 	@Mock
 	private Theme theme;
 
+	@Mock
+	private HttpRequest httpRequest;
+
+	@Mock
+	private MultivaluedMap<String, String> formParameters;
+
+	@Mock
+	private org.keycloak.models.SubjectCredentialManager credentialManager;
+
+	@Mock
+	private CredentialModel credentialModel;
+
+	@Mock
+	private AuthenticatorConfigModel authenticatorConfig;
+
+	@Mock
+	private AuthenticationSessionModel authSession;
+
+	@Mock
+	private ThemeManager themeManager;
+
+	@Mock
+	private KeycloakContext keycloakContext;
+
+	@Mock
+	private Locale locale;
+
+	@Mock
+	private Properties property;
+
+	@Mock
+	private LoginFormsProvider loginFormsProvider;
+
+	@Mock
+	private Response challenge;
+
 	@BeforeEach
 	void setUp() {
 		authenticator = new SmsAuthenticator();
@@ -75,7 +121,7 @@ class SmsAuthenticatorTest {
 	@Test
 	@DisplayName("should have correct TPL_CODE constant")
 	void shouldHaveCorrectTplCodeConstant() {
-		assertEquals("login-sms.ftl", SmsAuthenticator.TPL_CODE);
+		assertEquals("login-sms.ftl", TPL_CODE);
 	}
 
 	@Test
@@ -111,9 +157,51 @@ class SmsAuthenticatorTest {
 	}
 
 	@Test
-	@DisplayName("getType should return mobile-number type")
-	void getTypeShouldReturnMobileNumberType() {
-		SmsAuthCredentialModel.TYPE.equals("mobile-number");
-		assertEquals("mobile-number", SmsAuthCredentialModel.TYPE);
+	@DisplayName("authenticate should validate SMS code and proceed on success")
+	void authenticateShouldValidateSmsCodeAndProceedOnSuccess() throws java.io.IOException {
+		String mobileNumber = "+491761234567";
+		String credentialData = "{\"mobile_number\":\"" + mobileNumber + "\"}";
+
+		when(user.credentialManager()).thenReturn(credentialManager);
+		when(credentialManager.getStoredCredentialsByTypeStream(eq(SmsAuthCredentialModel.TYPE))).thenReturn(Stream.of(credentialModel));
+		when(credentialModel.getCredentialData()).thenReturn(credentialData);
+		when(context.getUser()).thenReturn(user);
+		when(context.getRealm()).thenReturn(realm);
+		when(context.getAuthenticatorConfig()).thenReturn(authenticatorConfig);
+		when(authenticatorConfig.getConfig()).thenReturn(Map.of("length", "6", "ttl", "300"));
+		when(context.getAuthenticationSession()).thenReturn(authSession);
+		when(context.getSession()).thenReturn(session);
+		when(session.theme()).thenReturn(themeManager);
+		when(themeManager.getTheme(eq(Theme.Type.LOGIN))).thenReturn(theme);
+		when(session.getContext()).thenReturn(keycloakContext);
+		when(keycloakContext.resolveLocale(user)).thenReturn(locale);
+		when(theme.getEnhancedMessages(eq(realm), eq(locale))).thenReturn(property);
+		when(property.getProperty(eq("smsAuthText"))).thenReturn("");
+		when(context.form()).thenReturn(loginFormsProvider);
+		when(loginFormsProvider.setAttribute(eq("realm"), eq(realm))).thenReturn(loginFormsProvider);
+		when(loginFormsProvider.createForm(TPL_CODE)).thenReturn(challenge);
+
+		authenticator.authenticate(context);
+
+		verify(context).challenge(eq(challenge));
+	}
+
+	@Test
+	@DisplayName("action should proceed on valid SMS code")
+	void actionShouldProceedOnValidSmsCode() {
+		String enteredCode = "123456";
+		String storedCode = "123456";
+		String ttl = String.valueOf(System.currentTimeMillis() + 300000L);
+
+		when(context.getHttpRequest()).thenReturn(httpRequest);
+		when(httpRequest.getDecodedFormParameters()).thenReturn(formParameters);
+		when(formParameters.getFirst("code")).thenReturn(enteredCode);
+		when(context.getAuthenticationSession()).thenReturn(authSession);
+		when(authSession.getAuthNote("code")).thenReturn(storedCode);
+		when(authSession.getAuthNote("ttl")).thenReturn(ttl);
+
+		authenticator.action(context);
+
+		verify(context).success();
 	}
 }
